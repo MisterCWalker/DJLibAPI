4df72b9fbed19305c9be9bd4c34ad817b2fe9bce
Craig Walker <craig.2.walker@worldline.com>
2023-09-14 21:32:56 +0100

    Moving to cloud native

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.5.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.16.2"
+    }
+  }
+  backend "s3" {
+    bucket = "dj-library-terraform"
+    key    = "terraform/state"
+    region = "eu-west-2"
+  }
+}
